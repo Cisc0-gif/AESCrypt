bb14dede768d8c6ee0678fa06168353b9b3e8092
Ferenc Gozony <root@kali>
2021-02-03 02:07:35 -0500

    Fixed missing comma in lib install

--- aescrypt.py
+++ aescrypt.py
@@ -9,7 +9,7 @@ if os.geteuid() != 0:
   print("Please run with sudo...")
   exit()
 
-reqs = subprocess.check_output([sys.executable, '-m' 'pip', 'freeze'])
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 if 'pyAesCrypt' not in installed_packages:
   print("Package 'pyAesCrypt' not installed, installing now...")
